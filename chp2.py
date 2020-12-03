0839d2a2a2d7ded2cb2e1c512727a765bb422922
Syeda Maham Fatima <61540404+mahumsyed@users.noreply.github.com>
2020-12-03 11:26:57 +0500

    first commit

---
+++ chp2.py
@@ -0,0 +1,5 @@
+var1=input("Enter your number to continue..") #input function use as taking input as string
+print(var1)
+var2=int(input("i'm giving you output as int")) #input function use as taking input as int
+print(var2)
+print("maham " * 3)  #for multiply print
---
+++ hello.py
@@ -0,0 +1,17 @@
+print(" Syeda Maham Fatima ")
+print("This is \\\\ Doubleslash") 
+print("These are /\/\/\ mountains")
+print("He is \t awesome")
+print(" \\\" \\t \\n \\'")
+print(r"maham is a good \n girl")
+print("Maham is a good \n girl")
+print("\U0001F60C")  #use / and + ki jaga teen 0
+print(2+4) #plus
+print(2/4) #ans in float
+print(4//2) #ans in int
+print(2%4) # de ga reminder
+print(2**4) #Exponent power
+print(2-4) #minus
+print(round(2**0.5,4)) #for round off
+print(2**0.5) #without roundoff
+print("maham is good girl")
